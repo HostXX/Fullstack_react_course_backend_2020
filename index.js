79f8048d85798eaeb02663a9d79ebbf16fb4ed26
Carlos Garcia <carlosmauriciogarcia13@gmail.com>
2020-10-21 18:11:00 -0400

    finished 3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express()
 let persons = require('./personsDb')
 require('dotenv').config()
 
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 50000)
+    return id
+  }
+
+
+
 app.use(express.json())
 const PORT = process.env.APP_PORT
 
@@ -49,6 +57,27 @@ app.delete('/api/person/:id', (req, res) => {
   persons = persons.filter(person => person.id !== id)
 
   return res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    // if (typeof body.content !== 'String' && body.content === '') {
+    //   console.log('is not a string or is empty')
+    //   return res.status(400).json({
+    //     error: 'Content missing or bad content'
+    //   })
+    // }
+
+    const newPerson = {
+      name: body.name,
+      phone: body.phone,
+      id: generateId()
+    }
+
+    persons = persons.concat(newPerson)
+    res.json(newPerson).end()
+ 
 })
 
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`))
--- personsDb.js
+++ personsDb.js
@@ -3,13 +3,11 @@ let persons  = [
         "name": "roberto arias",
         "phone": "555445445",
         "id": 0,
-        "number": "745"
       },
       {
         "name": "sitrus magnus",
         "phone": "212312314141",
         "id": 1,
-        "number": ""
       },
       {
         "name": "viel",
