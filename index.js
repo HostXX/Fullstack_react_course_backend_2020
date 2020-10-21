c9ac007dfb589608d5b327a01c5408813e2dcaea
Carlos Garcia <carlosmauriciogarcia13@gmail.com>
2020-10-21 17:55:20 -0400

    finished 3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -8,36 +8,47 @@ const PORT = process.env.APP_PORT
 
 console.log(PORT)
 
-app.get('/',(req,res) => {
-    return res.send('<h1>Root</h1>')
+app.get('/', (req, res) => {
+  return res.send('<h1>Root</h1>')
 })
 
-app.get('/api/persons',(req,res) => {
-    console.log()
-    res.json(persons)
+app.get('/api/persons', (req, res) => {
+  console.log()
+  res.json(persons)
 })
 
-app.get('/api/info',(req,res) => {
-    console.log()
-    res.send(
-       `<h5> The phonebook has info for ${persons.length} people </h5>
+app.get('/api/info', (req, res) => {
+  console.log()
+  res.send(
+    `<h5> The phonebook has info for ${persons.length} people </h5>
         <h4>${new Date(Date.now())}</h4>
        `
-    )
+  )
 })
 
 app.get('/api/person/:id', (req, res) => {
-    const id = Number(req.params.id)
-    let person = persons.find(person => person.id === id)
-  
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  })
+  const id = Number(req.params.id)
+  let person = persons.find(person => person.id === id)
 
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
 
+app.delete('/api/person/:id', (req, res) => {
+  const id = Number(req.params.id)
 
+  const isDeleted = persons.find(person => person.id === id)
 
-app.listen(PORT,() => console.log(`App listening on port: ${PORT}`))
+  if (isDeleted === undefined) {
+    return res.status(204).end()
+  }
+
+  persons = persons.filter(person => person.id !== id)
+
+  return res.status(204).end()
+})
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`))
